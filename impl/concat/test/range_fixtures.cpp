a680d5b6e10fed81e58141db9d69740ce71884e8
slymz <slymz@users.noreply.github.com>
2022-01-26 02:23:16 +0000

    Deploying to gh-pages from @ huixie90/cpp_papers@0df11c2de8a1452132eb05b498e1a1359abb2dd3 🚀

--- impl/concat/test/range_fixtures.cpp
+++ impl/concat/test/range_fixtures.cpp
@@ -75,7 +75,6 @@ concept cpp17_random_access_iterator = cpp17_bidirectional_iterator<I> && totall
 
 
 
-
 TEST_POINT("Cpp17OutputIter") {
     using It = Cpp17OutputIter;
     using ItCat = std::iterator_traits<It>::iterator_category;
@@ -109,9 +108,12 @@ TEST_POINT("Cpp17InputIter") {
     STATIC_CHECK(std::cpp17_iterator<It>);
     STATIC_CHECK(std::cpp17_input_iterator<It>);
     STATIC_CHECK(std::same_as<ItCat, std::input_iterator_tag>);
+
     STATIC_CHECK(std::derived_from<ItCat, std::input_iterator_tag>);
     STATIC_CHECK(!std::derived_from<ItCat, std::output_iterator_tag>);
     STATIC_CHECK(!std::derived_from<ItCat, std::forward_iterator_tag>);
+    STATIC_CHECK(!std::derived_from<ItCat, std::bidirectional_iterator_tag>);
+    STATIC_CHECK(!std::derived_from<ItCat, std::random_access_iterator_tag>);
 
     STATIC_CHECK(!std::indirectly_writable<It, int>);
     STATIC_CHECK(std::indirectly_readable<It>);
@@ -122,6 +124,189 @@ TEST_POINT("Cpp17InputIter") {
     STATIC_CHECK(std::input_iterator<It>);
     STATIC_CHECK(!std::output_iterator<It, int>);
     STATIC_CHECK(!std::forward_iterator<It>);
+    STATIC_CHECK(!std::bidirectional_iterator<It>);
+    STATIC_CHECK(!std::random_access_iterator<It>);
+}
+
+
+
+TEST_POINT("Cpp17ForwardIter") {
+    using It = Cpp17ForwardIter;
+    using ItCat = std::iterator_traits<It>::iterator_category;
+
+    STATIC_CHECK(std::cpp17_iterator<It>);
+    STATIC_CHECK(std::cpp17_input_iterator<It>);
+    STATIC_CHECK(std::same_as<ItCat, std::forward_iterator_tag>);
+
+    STATIC_CHECK(std::derived_from<ItCat, std::input_iterator_tag>);
+    STATIC_CHECK(!std::derived_from<ItCat, std::output_iterator_tag>);
+    STATIC_CHECK(std::derived_from<ItCat, std::forward_iterator_tag>);
+    STATIC_CHECK(!std::derived_from<ItCat, std::bidirectional_iterator_tag>);
+    STATIC_CHECK(!std::derived_from<ItCat, std::random_access_iterator_tag>);
+
+    STATIC_CHECK(!std::indirectly_writable<It, int>);
+    STATIC_CHECK(std::indirectly_readable<It>);
+    STATIC_CHECK(std::weakly_incrementable<It>);
+    STATIC_CHECK(std::incrementable<It>);
+    STATIC_CHECK(std::input_or_output_iterator<It>);
+    STATIC_CHECK(std::sentinel_for<It, It>);
+    STATIC_CHECK(std::input_iterator<It>);
+    STATIC_CHECK(!std::output_iterator<It, int>);
+    STATIC_CHECK(std::forward_iterator<It>);
+    STATIC_CHECK(!std::bidirectional_iterator<It>);
+    STATIC_CHECK(!std::random_access_iterator<It>);
+}
+
+
+TEST_POINT("Cpp17ForwardMutableIter") {
+    using It = Cpp17ForwardMutableIter;
+    using ItCat = std::iterator_traits<It>::iterator_category;
+
+    STATIC_CHECK(std::cpp17_iterator<It>);
+    STATIC_CHECK(std::cpp17_input_iterator<It>);
+    STATIC_CHECK(std::same_as<ItCat, std::forward_iterator_tag>);
+
+    STATIC_CHECK(std::derived_from<ItCat, std::input_iterator_tag>);
+    STATIC_CHECK(!std::derived_from<ItCat, std::output_iterator_tag>); // what'd be correct?
+    STATIC_CHECK(std::derived_from<ItCat, std::forward_iterator_tag>);
+    STATIC_CHECK(!std::derived_from<ItCat, std::bidirectional_iterator_tag>);
+    STATIC_CHECK(!std::derived_from<ItCat, std::random_access_iterator_tag>);
+
+    STATIC_CHECK(std::indirectly_writable<It, int>);
+    STATIC_CHECK(std::indirectly_readable<It>);
+    STATIC_CHECK(std::weakly_incrementable<It>);
+    STATIC_CHECK(std::incrementable<It>);
+    STATIC_CHECK(std::input_or_output_iterator<It>);
+    STATIC_CHECK(std::sentinel_for<It, It>);
+    STATIC_CHECK(std::input_iterator<It>);
+    STATIC_CHECK(std::output_iterator<It, int>);
+    STATIC_CHECK(std::forward_iterator<It>);
+    STATIC_CHECK(!std::bidirectional_iterator<It>);
+    STATIC_CHECK(!std::random_access_iterator<It>);
+}
+
+
+
+TEST_POINT("Cpp17BidirectionalIter") {
+    using It = Cpp17BidirectionalIter;
+    using ItCat = std::iterator_traits<It>::iterator_category;
+
+    STATIC_CHECK(std::cpp17_iterator<It>);
+    STATIC_CHECK(std::cpp17_input_iterator<It>);
+    STATIC_CHECK(std::same_as<ItCat, std::bidirectional_iterator_tag>);
+
+    STATIC_CHECK(std::derived_from<ItCat, std::input_iterator_tag>);
+    STATIC_CHECK(!std::derived_from<ItCat, std::output_iterator_tag>);
+    STATIC_CHECK(std::derived_from<ItCat, std::forward_iterator_tag>);
+    STATIC_CHECK(std::derived_from<ItCat, std::bidirectional_iterator_tag>);
+    STATIC_CHECK(!std::derived_from<ItCat, std::random_access_iterator_tag>);
+
+    STATIC_CHECK(std::indirectly_writable<It, int>);
+    STATIC_CHECK(std::indirectly_readable<It>);
+    STATIC_CHECK(std::weakly_incrementable<It>);
+    STATIC_CHECK(std::incrementable<It>);
+    STATIC_CHECK(std::input_or_output_iterator<It>);
+    STATIC_CHECK(std::sentinel_for<It, It>);
+    STATIC_CHECK(std::input_iterator<It>);
+    STATIC_CHECK(std::output_iterator<It, int>);
+    STATIC_CHECK(std::forward_iterator<It>);
+    STATIC_CHECK(std::bidirectional_iterator<It>);
+    STATIC_CHECK(!std::random_access_iterator<It>);
+}
+
+
+
+template <typename T>
+concept has_iterator_category = requires() {
+    typename T::iterator_category;
+};
+
+
+TEST_POINT("Cpp20InputIter") {
+    using It = Cpp20InputIter;
+
+    STATIC_CHECK(!has_iterator_category<std::iterator_traits<It>>);
+
+    STATIC_CHECK(!std::cpp17_iterator<It>);
+
+    STATIC_CHECK(!std::indirectly_writable<It, int>); // rvalue reference
+    STATIC_CHECK(std::indirectly_readable<It>);
+    STATIC_CHECK(std::weakly_incrementable<It>);
+    STATIC_CHECK(!std::incrementable<It>); // void postfix
+    STATIC_CHECK(std::input_or_output_iterator<It>);
+    STATIC_CHECK(std::sentinel_for<It, It>);
+    STATIC_CHECK(std::input_iterator<It>);
+    STATIC_CHECK(!std::output_iterator<It, int>);
+    STATIC_CHECK(!std::forward_iterator<It>);
+    STATIC_CHECK(!std::bidirectional_iterator<It>);
+    STATIC_CHECK(!std::random_access_iterator<It>);
+}
+
+TEST_POINT("Cpp20InputCpp17OutputIter") {
+    using It = Cpp20InputCpp17OutputIter;
+
+    STATIC_CHECK(
+        std::same_as<std::iterator_traits<It>::iterator_category, std::output_iterator_tag>);
+
+    STATIC_CHECK(std::cpp17_iterator<It>);
+    STATIC_CHECK(!std::cpp17_input_iterator<It>);
+
+    STATIC_CHECK(!std::indirectly_writable<It, int>); // rvalue reference
+    STATIC_CHECK(std::indirectly_readable<It>);
+    STATIC_CHECK(std::weakly_incrementable<It>);
+    STATIC_CHECK(!std::incrementable<It>); // no It == It
+    STATIC_CHECK(std::input_or_output_iterator<It>);
+    STATIC_CHECK(std::sentinel_for<It::Sentinel, It>);
+    STATIC_CHECK(std::input_iterator<It>);
+    STATIC_CHECK(!std::output_iterator<It, int>);
+    STATIC_CHECK(!std::forward_iterator<It>);
+    STATIC_CHECK(!std::bidirectional_iterator<It>);
+    STATIC_CHECK(!std::random_access_iterator<It>);
+}
+
+
+
+TEST_POINT("Cpp20ForwardIter") {
+    using It = Cpp20ForwardIter;
+
+    STATIC_CHECK(
+        std::same_as<std::iterator_traits<It>::iterator_category, std::input_iterator_tag>);
+
+    STATIC_CHECK(std::cpp17_input_iterator<It>);
+    STATIC_CHECK(!std::cpp17_forward_iterator<It>);
+
+    STATIC_CHECK(!std::indirectly_writable<It, int>); // rvalue reference
+    STATIC_CHECK(std::indirectly_readable<It>);
+    STATIC_CHECK(std::weakly_incrementable<It>);
+    STATIC_CHECK(std::incrementable<It>);
+    STATIC_CHECK(std::input_or_output_iterator<It>);
+    STATIC_CHECK(std::sentinel_for<It, It>);
+    STATIC_CHECK(std::input_iterator<It>);
+    STATIC_CHECK(!std::output_iterator<It, int>);
+    STATIC_CHECK(std::forward_iterator<It>);
+    STATIC_CHECK(!std::bidirectional_iterator<It>);
+    STATIC_CHECK(!std::random_access_iterator<It>);
+}
+
+
+TEST_POINT("Cpp20ForwardIterWithTraits") {
+    using It = Cpp20ForwardIterWithTraits;
+
+    STATIC_CHECK(
+        std::same_as<std::iterator_traits<It>::iterator_category, std::input_iterator_tag>);
+
+    STATIC_CHECK(std::cpp17_input_iterator<It>);
+    STATIC_CHECK(!std::cpp17_forward_iterator<It>);
+
+    STATIC_CHECK(!std::indirectly_writable<It, int>); // rvalue reference
+    STATIC_CHECK(std::indirectly_readable<It>);
+    STATIC_CHECK(std::weakly_incrementable<It>);
+    STATIC_CHECK(std::incrementable<It>);
+    STATIC_CHECK(std::input_or_output_iterator<It>);
+    STATIC_CHECK(std::sentinel_for<It, It>);
+    STATIC_CHECK(std::input_iterator<It>);
+    STATIC_CHECK(!std::output_iterator<It, int>);
+    STATIC_CHECK(std::forward_iterator<It>);
     STATIC_CHECK(!std::bidirectional_iterator<It>);
     STATIC_CHECK(!std::random_access_iterator<It>);
 }
--- impl/concat/test/range_fixtures.hpp
+++ impl/concat/test/range_fixtures.hpp
@@ -1,6 +1,9 @@
 #pragma once
-#include <cstddef>
-#include <xutility>
+#include <iterator>
+
+//  +-------------------+
+//  |  Cpp17OutputIter  |
+//  +-------------------+
 
 struct Cpp17OutputIter {
     using It = int*;
@@ -22,6 +25,11 @@ struct Cpp17OutputIter {
     using difference_type = std::ptrdiff_t; // for incrementable_traits
 };
 
+
+//  +------------------+
+//  |  Cpp17InputIter  |
+//  +------------------+
+
 struct Cpp17InputIter {
     using It = int*;
     It it;
@@ -38,10 +46,196 @@ struct Cpp17InputIter {
 
     using value_type = int;                 // to model indirectly_readable_traits
     using difference_type = std::ptrdiff_t; // to model incrementable_traits
-    // using pointer = int*;
-    using reference = int&;
 
     // must pin iterator category, otherwise it models std::forward_iterator<It>
     using iterator_category = std::input_iterator_tag;
-    //using iterator_concept = std::input_iterator_tag; // also works
+    // using iterator_concept = std::input_iterator_tag; // also works
 };
+
+
+//  +--------------------+
+//  |  Cpp17ForwardIter  |
+//  +--------------------+
+
+struct Cpp17ForwardIter {
+    using It = int*;
+    It it;
+
+    int const& operator*() const { return *it; }
+
+    Cpp17ForwardIter& operator++() {
+        ++it;
+        return *this;
+    }
+    Cpp17ForwardIter operator++(int) { return {it++}; }
+
+    bool operator==(Cpp17ForwardIter other) const { return it == other.it; }
+
+    using value_type = int;                 // to model indirectly_readable_traits
+    using difference_type = std::ptrdiff_t; // to model incrementable_traits
+};
+
+
+//  +---------------------------+
+//  |  Cpp17ForwardMutableIter  |
+//  +---------------------------+
+struct Cpp17ForwardMutableIter {
+    using It = int*;
+    It it;
+
+    int& operator*() const { return *it; }
+
+    Cpp17ForwardMutableIter& operator++() {
+        ++it;
+        return *this;
+    }
+    Cpp17ForwardMutableIter operator++(int) { return {it++}; }
+
+    bool operator==(Cpp17ForwardMutableIter other) const { return it == other.it; }
+
+    using value_type = int;                 // to model indirectly_readable_traits
+    using difference_type = std::ptrdiff_t; // to model incrementable_traits
+};
+
+
+//  +--------------------------+
+//  |  Cpp17BidirectionalIter  |
+//  +--------------------------+
+struct Cpp17BidirectionalIter {
+    using It = int*;
+    It it;
+
+    int& operator*() const { return *it; }
+
+    Cpp17BidirectionalIter& operator++() {
+        ++it;
+        return *this;
+    }
+    Cpp17BidirectionalIter operator++(int) { return {it++}; }
+
+    Cpp17BidirectionalIter& operator--() {
+        --it;
+        return *this;
+    }
+    Cpp17BidirectionalIter operator--(int) { return {it--}; }
+
+
+    bool operator==(Cpp17BidirectionalIter other) const { return it == other.it; }
+
+    using value_type = int;                 // to model indirectly_readable_traits
+    using difference_type = std::ptrdiff_t; // to model incrementable_traits
+};
+
+
+
+//  +------------------+
+//  |  Cpp20InputIter  |
+//  +------------------+
+
+// void postfix, rvalue return
+struct Cpp20InputIter {
+    using It = int*;
+    It it;
+
+    int operator*() const { return *it; }
+
+    Cpp20InputIter& operator++() {
+        ++it;
+        return *this;
+    }
+    void operator++(int) { it++; }
+
+    bool operator==(Cpp20InputIter other) const { return it == other.it; }
+
+    using value_type = int;                 // to model indirectly_readable_traits
+    using difference_type = std::ptrdiff_t; // to model incrementable_traits
+};
+
+
+// regular postfix, rvalue return, BUT no comparison
+// - this is a c++20 input_iterator since it has sentinel comparison
+// - not a c++20 output_iterator since it has rvalue return
+// - and only a c++17 output_iterator, since it has no self comparison
+struct Cpp20InputCpp17OutputIter {
+    using It = int*;
+    struct Sentinel {
+        It it;
+    };
+    It it;
+
+    int operator*() const { return *it; }
+
+    Cpp20InputCpp17OutputIter& operator++() {
+        ++it;
+        return *this;
+    }
+    Cpp20InputCpp17OutputIter operator++(int) { return {it++}; }
+
+    bool operator==(Sentinel other) const { return it == other.it; }
+
+    using value_type = int;                 // to model indirectly_readable_traits
+    using difference_type = std::ptrdiff_t; // to model incrementable_traits
+};
+
+
+
+//  +--------------------+
+//  |  Cpp20ForwardIter  |
+//  +--------------------+
+
+struct Cpp20ForwardIter {
+    using It = int*;
+    It it;
+
+    int operator*() const { return *it; }
+
+    Cpp20ForwardIter& operator++() {
+        ++it;
+        return *this;
+    }
+    Cpp20ForwardIter operator++(int) { return {it++}; }
+
+    bool operator==(Cpp20ForwardIter other) const { return it == other.it; }
+
+    using value_type = int;                 // to model indirectly_readable_traits
+    using difference_type = std::ptrdiff_t; // to model incrementable_traits
+};
+
+
+
+struct Cpp20ForwardIterWithTraits {
+    using It = int*;
+    It it;
+
+    int operator*() const { return *it; }
+
+    Cpp20ForwardIterWithTraits& operator++() {
+        ++it;
+        return *this;
+    }
+    Cpp20ForwardIterWithTraits operator++(int) { return {it++}; }
+
+    bool operator==(Cpp20ForwardIterWithTraits other) const { return it == other.it; }
+
+    using value_type = int;                 // to model indirectly_readable_traits
+    using difference_type = std::ptrdiff_t; // to model incrementable_traits
+};
+
+
+namespace std {
+// without this specialization, it works as expected: C++17 input, and C++20 forward iterator
+//
+// with this specialization (which isn't necessary here, but in case it was necessary for other
+// implementation detail, such as the counting_iterator case which is disabling operator->) it would
+// end up being in the same category as what it inherits (input_iterator_tag here). But we *really*
+// want it to be C++20 forward iterator since it really is.
+
+template <>
+struct iterator_traits<Cpp20ForwardIterWithTraits> : iterator_traits<Cpp20ForwardIter> {
+    using iterator_concept = forward_iterator_tag;
+    // if this wasn't defined
+    // std::foward_iterator<Cpp20ForwardIterWithTraits> would fail
+    // we can not define iterator_category because it should stay as input_iterator_tag for C++17
+    // (rvalue reference)
+};
+} // namespace std
