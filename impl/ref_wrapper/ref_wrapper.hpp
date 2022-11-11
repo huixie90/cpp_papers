f43218ce66f60511c6abccdce4882f8bed725e37
slymz <slymz@users.noreply.github.com>
2022-11-11 07:04:33 +0000

    Deploying to gh-pages from @ huixie90/cpp_papers@537ddc3984a8696141527d810216d6e4caeea36d 🚀

--- impl/ref_wrapper/ref_wrapper.hpp
+++ impl/ref_wrapper/ref_wrapper.hpp
@@ -13,22 +13,23 @@ inline constexpr bool is_ref_wrapper = false;
 template <class T>
 inline constexpr bool is_ref_wrapper<reference_wrapper<T>> = true;
 
+// clang-format off
 
 template<class R, class T, class RQ, class TQ>
 concept specialization_constraint = // CRW macro
-    // C1. first type is a reference_wrapper.
-    is_ref_wrapper<R> //  && R is cv-unqualified omitted for simplicit. it already is.
-    // C2. T is cv-unqualified. omit same.
-    // C3.common_reference with the underlying type exists
-    && requires {    
+    // [1.1] first type is a reference_wrapper.
+    is_ref_wrapper<R>
+    // [1.2] common_reference with the underlying type exists
+    && requires {
         typename common_reference_t<typename R::type&, TQ>;
     }
-    // C4. reference_wrapper as specified is convertible to the above
+    // [1.3] reference_wrapper as specified is convertible to the above
     && convertible_to<RQ, common_reference_t<typename R::type&, TQ>>
     ;
 
 template <class R, class T, template <class> class RQual,  template <class> class TQual>
     requires(  specialization_constraint<R, T, RQual<R>, TQual<T>> 
+           // [1.4] commuted constraint should not match:
            && !specialization_constraint<T, R, TQual<T>, RQual<R>>  )
 struct basic_common_reference<R, T, RQual, TQual> {
     using type = common_reference_t<typename R::type&, TQual<T>>;
@@ -37,10 +38,13 @@ struct basic_common_reference<R, T, RQual, TQual> {
 
 template <class T, class R, template <class> class TQual,  template <class> class RQual>
     requires(  specialization_constraint<R, T, RQual<R>, TQual<T>> 
+           // [1.4] commuted constraint should not match:
            && !specialization_constraint<T, R, TQual<T>, RQual<R>>  )
 struct basic_common_reference<T, R, TQual, RQual> {
     using type = common_reference_t<typename R::type&, TQual<T>>;
 };
+
+// clang-format on
 
 } // namespace std
 
