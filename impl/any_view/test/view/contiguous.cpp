f8b91a26b6ce52b5e5029ea59c41ebf2a3ca72c3
Hui <hui.xie0621@gmail.com>
2024-05-29 17:45:15 +0100

    initial implementation

---
+++ impl/any_view/test/view/contiguous.cpp
@@ -0,0 +1,166 @@
+
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <catch2/catch_test_macros.hpp>
+
+#include "any_view.hpp"
+
+#define TEST_POINT(x) TEST_CASE(x, "[contiguous_view]")
+
+namespace {
+
+using AnyView =
+    std::ranges::any_view<int&, std::ranges::category::contiguous |
+                                    std::ranges::category::move_only_view>;
+
+static_assert(std::ranges::contiguous_range<AnyView>);
+static_assert(std::movable<AnyView>);
+static_assert(!std::copyable<AnyView>);
+static_assert(!std::ranges::sized_range<AnyView>);
+static_assert(!std::ranges::common_range<AnyView>);
+static_assert(!std::ranges::borrowed_range<AnyView>);
+
+using AnyViewFull = std::ranges::any_view<
+    int&, std::ranges::category::contiguous | std::ranges::category::sized |
+              std::ranges::category::common | std::ranges::category::borrowed>;
+
+static_assert(std::ranges::contiguous_range<AnyViewFull>);
+static_assert(std::movable<AnyViewFull>);
+static_assert(std::copyable<AnyViewFull>);
+static_assert(std::ranges::sized_range<AnyViewFull>);
+static_assert(std::ranges::common_range<AnyViewFull>);
+static_assert(std::ranges::borrowed_range<AnyViewFull>);
+
+template <class V>
+constexpr void basic() {
+  std::array v{1, 2, 3, 4, 5};
+
+  V view(std::views::all(v));
+
+  auto it = view.begin();
+  assert(*it == 1);
+
+  auto st = view.end();
+  assert(it != st);
+
+  ++it;
+  ++it;
+  ++it;
+  ++it;
+  ++it;
+
+  assert(it == st);
+}
+
+template <class V>
+constexpr void forward() {
+  std::array v{1, 2, 3, 4, 5};
+  V view(std::views::all(v));
+  auto it1 = view.begin();
+  ++it1;
+  assert(*it1 == 2);
+
+  auto it2 = view.begin();
+  assert(*it2 == 1);
+
+  it2 = it1;
+  assert(*it2 == 2);
+}
+
+template <class V>
+constexpr void bidirectional() {
+  std::array v{1, 2, 3, 4, 5};
+  V view(std::views::all(v));
+  auto it1 = view.begin();
+  ++it1;
+  assert(*it1 == 2);
+
+  --it1;
+  assert(*it1 == 1);
+}
+
+template <class V>
+constexpr void random_access() {
+  std::array v{1, 2, 3, 4, 5};
+  V view(std::views::all(v));
+  auto it1 = view.begin();
+  it1 += 4;
+  assert(*it1 == 5);
+
+  auto it2 = it1 - 3;
+  assert(*it2 == 2);
+}
+
+template <class V>
+constexpr void contiguous() {
+  std::array v{1, 2, 3, 4, 5};
+  V view(std::views::all(v));
+  std::same_as<int*> decltype(auto) it1 = view.begin();
+  assert(it1 == v[0]);
+}
+
+template <class V>
+constexpr void move() {
+  std::array v1{1, 2, 3, 4, 5};
+  std::array v2{3};
+
+  V view1(std::views::all(v1));
+  V view2(std::views::all(v2));
+
+  V view3(std::move(view1));
+
+  assert(*view3.begin() == 1);
+
+  view3 = std::move(view2);
+  assert(*view3.begin() == 3);
+}
+
+template <class V>
+constexpr void copy() {
+  std::array v1{1, 2, 3, 4, 5};
+  std::array v2{3};
+
+  V view1(std::views::all(v1));
+  V view2(std::views::all(v2));
+
+  V view3(view1);
+
+  assert(*view1.begin() == 1);
+  assert(*view3.begin() == 1);
+
+  view3 = view2;
+  assert(*view2.begin() == 3);
+  assert(*view3.begin() == 3);
+}
+
+template <class V>
+constexpr void size() {
+  std::array v1{1, 2, 3, 4, 5};
+
+  V view1(std::views::all(v1));
+  assert(view1.size() == 5);
+}
+
+constexpr bool test() {
+  basic<AnyView>();
+  basic<AnyViewFull>();
+  forward<AnyView>();
+  forward<AnyViewFull>();
+  bidirectional<AnyView>();
+  bidirectional<AnyViewFull>();
+  random_access<AnyView>();
+  random_access<AnyViewFull>();
+  move<AnyView>();
+  move<AnyViewFull>();
+  copy<AnyViewFull>();
+  size<AnyViewFull>();
+  return true;
+}
+
+TEST_POINT("forward") {
+  test();
+  static_assert(test());
+}
+
+}  // namespace
---
+++ impl/any_view/test/view/input.cpp
@@ -0,0 +1,117 @@
+
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <catch2/catch_test_macros.hpp>
+
+#include "any_view.hpp"
+
+#define TEST_POINT(x) TEST_CASE(x, "[input_view]")
+
+namespace {
+
+using AnyView =
+    std::ranges::any_view<int&, std::ranges::category::input |
+                                    std::ranges::category::move_only_view>;
+
+static_assert(std::ranges::input_range<AnyView>);
+static_assert(!std::ranges::forward_range<AnyView>);
+static_assert(std::movable<AnyView>);
+static_assert(!std::copyable<AnyView>);
+static_assert(!std::ranges::sized_range<AnyView>);
+static_assert(!std::ranges::common_range<AnyView>);
+static_assert(!std::ranges::borrowed_range<AnyView>);
+
+using AnyViewFull = std::ranges::any_view<
+    int&, std::ranges::category::input | std::ranges::category::sized |
+              std::ranges::category::common | std::ranges::category::borrowed>;
+
+static_assert(std::ranges::input_range<AnyViewFull>);
+static_assert(!std::ranges::forward_range<AnyViewFull>);
+static_assert(std::movable<AnyViewFull>);
+static_assert(std::copyable<AnyViewFull>);
+static_assert(std::ranges::sized_range<AnyViewFull>);
+static_assert(std::ranges::common_range<AnyViewFull>);
+static_assert(std::ranges::borrowed_range<AnyViewFull>);
+
+template <class V>
+constexpr void basic() {
+  std::array v{1, 2, 3, 4, 5};
+
+  V view(std::views::all(v));
+
+  auto it = view.begin();
+  assert(*it == 1);
+
+  auto st = view.end();
+  assert(it != st);
+
+  ++it;
+  ++it;
+  ++it;
+  ++it;
+  ++it;
+
+  assert(it == st);
+}
+
+template <class V>
+constexpr void move() {
+  std::array v1{1, 2, 3, 4, 5};
+  std::array v2{3};
+
+  V view1(std::views::all(v1));
+  V view2(std::views::all(v2));
+
+  V view3(std::move(view1));
+
+  assert(*view3.begin() == 1);
+
+  view3 = std::move(view2);
+  assert(*view3.begin() == 3);
+}
+
+template <class V>
+constexpr void copy() {
+  std::array v1{1, 2, 3, 4, 5};
+  std::array v2{3};
+
+  V view1(std::views::all(v1));
+  V view2(std::views::all(v2));
+
+  V view3(view1);
+
+  // std::ranges::end(view3);
+
+  assert(*view1.begin() == 1);
+  assert(*view3.begin() == 1);
+
+  view3 = view2;
+  assert(*view2.begin() == 3);
+  assert(*view3.begin() == 3);
+}
+
+template <class V>
+constexpr void size() {
+  std::array v1{1, 2, 3, 4, 5};
+
+  V view1(std::views::all(v1));
+  assert(view1.size() == 5);
+}
+
+constexpr bool test() {
+  basic<AnyView>();
+  basic<AnyViewFull>();
+  move<AnyView>();
+  move<AnyViewFull>();
+  copy<AnyViewFull>();
+  size<AnyViewFull>();
+  return true;
+}
+
+TEST_POINT("input") {
+  test();
+  static_assert(test());
+}
+
+}  // namespace
